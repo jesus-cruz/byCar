173e39cd281e32fa17bce8e2bb6cd941e66b884c
Manuel Fidalgo <m.fidalgo.fierro@gmail.com>
2017-06-07 23:55:28 +0200

    Terminada implementacion del chat

--- backendPHP/chat.php
+++ backendPHP/chat.php
@@ -3,8 +3,9 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$databasename = "busseatsmanager";
+$databasename = "bycardb";
 
+session_start();
 
 $GLOBALS['conn'] = new mysqli($servername, $username, $password, $databasename);
 // Check connection
@@ -15,23 +16,44 @@ if ($GLOBALS['conn']->connect_error) {
 if(isset($_POST['action']) && !empty($_POST['action'])) {
 
 	$action = $_POST['action'];
+
 	switch($action) {
-		case "SEND_MESSAGE": sendMessage($_POST['message'], $_POST['user_id_0'], $_POST["user_id_1"]); break;
-		case "GET_MESSAGES": getMessagesList($_POST['user_id_0'], $_POST["user_id_1"]); break;
+		case "SET_MESSAGE": sendMessage($_POST['message'], 2 , $_POST["user_id_1"]); //$_SESSION['id']; //HARDCOREADO PARA PRUEBAS
+		break;
+		case "GET_MESSAGES": getMessagesList(2 , $_POST["user_id_1"]); //$_SESSION['id']; //HARDCOREADO PARA PRUEBAS
+		break;
 	}
 	
-	public function sendMessage($message,$id_0, $id_1)
-	{
-		$sql = "insert into...";
-		$GLOBALS['conn']->querry($sql); //Inserta el mensaje enviado en la BBDD
-	}
+}
+$GLOBALS['conn']->close();
 
-	public function getMessagesList($id_0, $id_1)
-	{
-		$sql = "SELECT FROM mensages WHERE origen =" . $id_0 . "AND destino=" . $id_1 . "'";
-		$list = $GLOBALS['conn']->querry($sql);
+function sendMessage($message,$from, $to)
+{
 
-		echo json_encode($list);
+	$message = substr($message, 0, 140);	//limitamos a 140 caracteres
+
+	$sql = "INSERT INTO Mensajes (origen, destino, contenido, horaMensaje) VALUES ('".$from."','".$to."','".$message."','".date("Y-m-d H:i:s")."')";
+
+	if($GLOBALS['conn']->query($sql)){ //Inserta el mensaje enviado en la BBDD
+		echo "Exito";
+	}else{
+		echo $GLOBALS['conn']->error;
 	}
+}
+
+function getMessagesList($from, $to)
+{
+
+	$sql = "SELECT * FROM Mensajes WHERE origen ='".$from."' AND destino='".$to."' OR origen ='".$to."' AND destino='".$from."'";
+	$result = $GLOBALS['conn']->query($sql);
+
+	$return_arr = array();
+
+	if ($result->num_rows > 0){
+		while ($row = mysqli_fetch_assoc($result)) {
+			array_push($return_arr, $row);
+		}
+	}
+	echo json_encode($return_arr);
 }
 ?>
--- frontendJS/controladorChat.js
+++ frontendJS/controladorChat.js
@@ -1,14 +1,20 @@
+var me = {};
+me.avatar = "http://sumedico.com/wp-content/uploads/2017/02/donald_trump_enfermedades.jpg";
+
+var you = {};
+you.avatar = "https://s-media-cache-ak0.pinimg.com/736x/76/47/9d/76479dd91dc55c2768ddccfc30a4fbf5.jpg";
+you.id = 1; //Harcodeado para testeo;
+
 $(document).ready(function(){
 	//Documento preparado
 	//Recuperamos mediante ajax todos los chats entre los usuarios
-	//getChatMessages();
+	getChatMessages(you.id);
 	//Evento para que el mensaje pase a la lista
-	insertMessageInList("asd", "Hola buenas, a que hora le viene bien?");
 	$(".mytext").on("keydown", function(event){
 		if (event.which == 13){
 			var text = $(this).val();
 			if (text !== ""){
-				insertMessageInList("me", text);              
+				writeMessage(text);              
 				$(this).val('');
 			}
 		}
@@ -16,59 +22,81 @@ $(document).ready(function(){
 });
 
 //id_0 -> id del usuario que ejecuta esta llamada
-function getChatMessages(id_0,id_1) {
+function getChatMessages(other_id) {
 	$.ajax({
 		type: "post",
 		url: "backendPHP/chat.php",
 		data: {	
 			action: "GET_MESSAGES", 
-			user_id_0:id_0, 
-			user_id_1:id_1},
-			dataType: "post",
-			cache: "false",
-			success:function(data){
-			//Have to-> decode the JSON
+			user_id_1: other_id,
+		},
+		dataType: "json",
+		cache: "false",
+		success:function(data){
 			decodeMessageListAsJSON(data);
 		},
 		error: function(XMLHttpRequest, textStatus, errorThrown){
-
+			alert(XMLHttpRequest.responseText + ";" + textStatus + "," + errorThrown);
 		}
 	});
 }
-/*agnade un mensajde de chat a la BBDD*/
-function addChatMessage(id_0,id_1,message_){
+/*agnade un mensajde de chat a la BBDD, la fecha se coge por PHP, se le pasa el ID del destinatario*/
+function insertChatMessageInDB(other_id,message_){
+
 	$.ajax({
 		type: "post",
 		url: "backendPHP/chat.php",
 		data: {	
-			action: "GET_MESSAGES", 
-			user_id_0:id_0, 
-			user_id_1:id_1,
-			message: message_},
-			dataType: "post",
-			cache: "false",
-			success:function(data){
+			action: "SET_MESSAGE", 
+			user_id_1: other_id,
+			message: message_ ,
+		},
+		dataType: "text",
+		cache: "false",
+		success:function(data){		
+			alert(data);
+		},
+		error: function(XMLHttpRequest, textStatus, errorThrown){
+			alert(XMLHttpRequest.responseText + ";" + textStatus + "," + errorThrown);
+		}
+	});
 
-			},
-			error: function(XMLHttpRequest, textStatus, errorThrown){
-
-			}
-		});
 }
 
 /*Descodifica y muestra por pantalla la lista de mensajes enviados*/
 function decodeMessageListAsJSON(data) {
-	// body...
+	var who = "";
+
+	for (var i = 0 ; i < data.length; i++) {
+		if(data[i].origen == you.id){
+			who = 'other';
+		}else{
+			who = 'me';
+		}
+		insertMessageInList(who, data[i].contenido,data[i].horaMensaje);
+	}
 }
 
-var me = {};
-me.avatar = "http://sumedico.com/wp-content/uploads/2017/02/donald_trump_enfermedades.jpg";
 
-var you = {};
-you.avatar = "https://s-media-cache-ak0.pinimg.com/736x/76/47/9d/76479dd91dc55c2768ddccfc30a4fbf5.jpg";
+/*Escribe un mensaje en la bbdd y lo inserta en la lista y */
+function writeMessage(text){
 
+	insertChatMessageInDB(you.id,text)
+	insertMessageInList("me",text);
+	
 
-/*Interta un mensaje en la pagina.*/
+}
+
+/*Solo para testear, funcion no necesario*/
+function writeMessageYOU(text){
+
+	if(insertChatMessageInDB(me.id,text)){
+		insertMessageInList("you",text);
+	}
+
+}
+
+/*Interta un mensaje en la lista visible*/
 function insertMessageInList(who, text, date){
 
 	var control = "";
@@ -83,31 +111,32 @@ function insertMessageInList(who, text, date){
 
 }
 
-function createMessageElementMe(text, date) {
+/*Crea un elemento de tipo mensage y lo añade a la lista*/
+function createMessageElementYou(text, date) {
 
-	var HTML_ = '<li style="width:100%">' +
-                        '<div class="msj macro">' +
-                        '<div class="avatar"><img class="img-circle" style="width:60px ; height:60px;" src="'+ me.avatar +'" /></div>' +
-                            '<div class="text text-l">' +
-                                '<p>'+ text +'</p>' +
-                                '<p><small>'+date+'</small></p>' +
-                            '</div>' +
-                        '</div>' +
-                    '</li>';
+	var HTML_ = '<li style="width:100%;">' +
+	'<div class="msj macro">' +
+	'<div class="avatar"><img class="img-circle" style="width:60px ; height:60px;" src="'+ you.avatar +'" /></div>' +
+	'<div class="text text-l">' +
+	'<p>'+ text +'</p>' +
+	'<p>'+ date +'</p>' +
+	'</div>' +
+	'</div>' +
+	'</li>';
 
 	addToList(HTML_);
 }
 
-function createMessageElementYou(text, date) {
+function createMessageElementMe(text, date) {
 
 	var HTML_ = '<li style="width:100%;">' +
-                        '<div class="msj-rta macro">' +
-                            '<div class="text text-r">' +
-                                '<p>'+ text +'</p>' +
-                                '<p><small>'+ date +'</small></p>' +
-                            '</div>' +
-                        '<div class="avatar" style="padding:0px 0px 0px 10px !important"><img class="img-circle" style="width:60px" src="'+ you.avatar+ '" /></div>' +                                
-                  '</li>';
+	'<div class="msj-rta macro">' +
+	'<div class="text text-r">' +
+	'<p>'+ text +'</p>' +
+	'<p>'+ date +'</p>' +
+	'</div>' +
+	'<div class="avatar" style="padding:0px 0px 0px 10px !important"><img class="img-circle" style="width:60px ; height:60px;" src="'+ me.avatar+ '" /></div>' +                                
+	'</li>';
 
 	addToList(HTML_); 
 }
