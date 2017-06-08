ba483fd9857b98758e75805d630b0038661571f5
jesus-cruz <cruzoliverajesus@gmail.com>
2017-06-08 10:47:36 +0200

    subir imagen

--- backendPHP/subirImagen.php
+++ backendPHP/subirImagen.php
@@ -1,33 +1,30 @@
 
 <?php
-$target_dir = "img/";
-$target_file = $target_dir . basename($_FILES["foto"]["name"]);
-$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+$dirImg = "img/";
+$imagenUsr = $dirImg . basename($_FILES["foto"]["name"]);
+$imagenScript = pathinfo($imagenUsr,PATHINFO_EXTENSION);
 // Check if image file is a actual image or fake image
 if(isset($_POST["submit"])) {
-    $check = getimagesize($_FILES["foto"]["tmp_name"]);
-    if($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".";
-    } else {
-        echo "File is not an image.";
+    if(!getimagesize($_FILES["foto"]["tmp_name"])) {
+        echo "No es una imagen";
+        return;
+    }
+    if ($_FILES["foto"]["size"] > 50000) {
+        echo "Imagen demasiado grande";
+        return;
+    }
+    if($imagenScript != "jpg" && $imagenScript != "png" && $imagenScript != "jpeg" ) {
+        echo "Error en el formato, solo están permitidos jpg, jpeg o png";
+        return;
     }
 }
 
-// Check file size
-if ($_FILES["foto"]["size"] > 500000) {
-    echo "Sorry, your file is too large.";
-}
-// Allow certain file formats
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-&& $imageFileType != "gif" ) {
-    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-}
+// La imagen es una imagen del tamaño apropiado y en un formato permitido
 
-
-    if (move_uploaded_file($_FILES["foto"]["tmp_name"], $target_file)) {
-        echo "The file ". basename( $_FILES["foto"]["name"]). " has been uploaded.";
-    } else {
-        echo "Sorry, there was an error uploading your file.";
-    }
+if (move_uploaded_file($_FILES["foto"]["tmp_name"], $imagenUsr)) {
+    echo "Archivo subido";
+} else {
+    echo "Error subiendo la imagen, pero era una imagen correcta";
+}
 
 ?>
